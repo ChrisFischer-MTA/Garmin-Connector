c920df1f109be706143fd38b9bb735669d38f1c8
Chris Fischer <christopherrfischer@outlook.com>
2025-06-01 07:20:54 -0400

    adding some caching to prevent location clustering

--- main.py
+++ main.py
@@ -31,10 +31,13 @@ def read_root():
 def getLocations():
     activities = ActivityRecords.get_all(garmin_act_db)
     returned_item = {}
+    map_locations = {}
     for element in activities:
         if element.position_lat is None or element.position_long is None:
             continue
-        returned_item[element.timestamp] = {'activity' : element.activity_id, 'position_lat' : element.position_lat, 'position_long' : element.position_long }
+        if map_locations.get(str(round(element.position_lat, 3)+'-'+(round(element.position_long, 3)))) is None:
+            map_locations[str(round(element.position_lat, 3)+'-'+(round(element.position_long, 3)))] = True
+            returned_item[element.timestamp] = {'activity' : element.activity_id, 'position_lat' : element.position_lat, 'position_long' : element.position_long }
     return returned_item
 
 @app.get("/updateDB")
